f7984b98d23607fa78856a20e638ca30b58b5b8a
Lucas <lukascoutinhoalmeida@gmail.com>
2024-11-19 16:50:33 -0300

    Adicionando testes para titles das páginas home, help e about

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @name_app = 'Amostra App'
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
@@ -14,5 +18,20 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get about" do
     get static_pages_about_url
     assert_response :success
+  end
+
+  test "should get home title" do
+    get static_pages_home_url
+    assert_select "title", "Home | #{ @name_app }"
+  end
+
+  test "should get help title" do
+    get static_pages_help_url
+    assert_select "title", "Help | #{ @name_app }"
+  end
+
+  test "should get about title" do
+    get static_pages_about_url
+    assert_select "title", "About | #{ @name_app }"
   end
 end
