c79ab91e58dfad99d307e9c31474cf82a143da8c
Lucas <lukascoutinhoalmeida@gmail.com>
2024-12-05 16:04:12 -0300

    Adicionando rotas, actions, formulário e teste de Sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:password])
+      
+    else
+      flash.now[:danger] = "Combinação de email e senha inválida"
+      render "new", status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+  end
 end
