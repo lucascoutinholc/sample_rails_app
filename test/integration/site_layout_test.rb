b1df62922ef931f0f8cbf58d100298a31a4364ed
Lucas <lukascoutinhoalmeida@gmail.com>
2024-12-17 12:39:29 -0300

    Adicionando teste de integração de todos os links da aplicação

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,6 +1,10 @@
 require "test_helper"
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
+  def setup
+    @user = users(:lucas)
+  end
+
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -13,5 +17,22 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "title", full_title("Contact")
     get signup_path
     assert_select "title", full_title("Signup")
+  end
+
+  test "verify all links" do
+    get root_path
+    assert_template "static_pages/home"
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", about_path, count: 2
+    assert_select "a[href=?]", contact_path, count: 2
+    assert_select "a[href=?]", help_path, count: 2
+    assert_select "a[href=?]", signup_path, count: 2
+    assert_select "a[href=?]", login_path
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", edit_user_path(@user)
+    assert_select "a[href=?]", logout_path
   end
 end
