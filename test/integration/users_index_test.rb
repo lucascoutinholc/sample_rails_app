dfab01fee37eb16c027f457e1af8f3ce7872d740
Lucas <lukascoutinhoalmeida@gmail.com>
2024-12-18 13:23:49 -0300

    Adicionando testes de integração para o controller de Users

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @user = users(:lucas)
+    @admin = @user
+    @non_admin = users(:joao)
   end
 
   test "index including pagination" do
@@ -13,5 +15,30 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     User.paginate(page: 1, per_page: 25).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
     end
+  end
+
+  test "index as admin including pagination and delete links" do
+    log_in_as(@admin)
+    get users_path
+    assert_template "users/index"
+    assert_select "div.pagination"
+    first_page_of_users = User.paginate(page: 1, per_page: 25)
+    first_page_of_users.each do |user|
+      assert_select "a[href=?]", user_path(user), text: user.name
+      unless user == @admin
+        assert_select "a[href=?]", user_path(user), text: "Excluir usuário"
+      end
+    end
+    assert_difference 'User.count', -1 do
+      delete user_path(@non_admin)
+      assert_response :see_other
+      assert_redirected_to users_path
+    end
+  end
+  
+  test "index as non-admin" do
+    log_in_as(@non_admin)
+    get users_path
+    assert_select "a", text: "Excluir usuário", count: 0
   end
 end
