77257f5d6f2cbf509c473ca40a7a6aa2c216bd25
Lucas <lukascoutinhoalmeida@gmail.com>
2024-12-19 16:37:40 -0300

    Adicionando métodos para criação de token de ativação da conta de Usuário

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,9 @@ User.create!(name: "Usuário de exemplo",
              email: "usuarioexemplo@example.com",
              password: "password",
              password_confirmation: "password",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
@@ -21,5 +23,7 @@ User.create!(name: "Usuário de exemplo",
   User.create(name: name,
               email: email,
               password: password,
-              password_confirmation: password)
+              password_confirmation: password,
+              activated: true,
+              activated_at: Time.zone.now)
 end
