0f0cb6ac3ea645caaf10dfa290921d80833fb402
Lucas <lukascoutinhoalmeida@gmail.com>
2024-12-23 16:41:25 -0300

    Adicionando o método edit no controller de AccountActivations e adicionando código para verificar se o usuário já foi ativado na hora do login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,19 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
 
     if @user&.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      reset_session
-      params[:session][:remember_me] == '1' ? 
-        remember(@user) : forget(@user)
-      log_in @user
-      redirect_to forwarding_url || @user
+      if @user.activated?
+        forwarding_url = session[:forwarding_url]
+        reset_session
+        params[:session][:remember_me] == '1' ? 
+          remember(@user) : forget(@user)
+        log_in @user
+        redirect_to forwarding_url || @user
+      else
+        message = "Conta não ativada. "
+        message += "Cheque seu email para acessar o link de ativação da conta!"
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = "Combinação de email e senha inválida"
       render "new", status: :unprocessable_entity
