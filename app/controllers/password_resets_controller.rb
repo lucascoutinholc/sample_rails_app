cc76cb9eae8a480c86f454e801dfb6415e01cec3
Lucas <lukascoutinhoalmeida@gmail.com>
2024-12-26 16:39:57 -0300

    Adicionando a action create para procurar o usuário pelo email e mandar o link de recuperação da senha

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -4,4 +4,18 @@ class PasswordResetsController < ApplicationController
 
   def edit
   end
+
+  def create
+    @user = User.find_by(email: params[:password_reset][:email].downcase)
+
+    if @user
+      @user.create_reset_digest
+      @user.send_password_reset_email
+      flash[:info] = "O link de alteração da senha foi enviado para o email informado."
+      redirect_to root_url
+    else
+      flash.now[:danger] = "O email informado não foi encontrado na base de dados."
+      render "new", status: :unprocessable_entity
+    end
+  end
 end
