5d31cf6004bf05043ae950e346f79729c1f2d4c7
Lucas <lukascoutinhoalmeida@gmail.com>
2024-12-27 14:20:26 -0300

    Melhorando o arquivo user_mailer_test.rb

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -5,12 +5,12 @@ class UserMailerTest < ActionMailer::TestCase
     user = users(:lucas)
     user.activation_token = User.new_token
     mail = UserMailer.account_activation(user)
-    assert_equal "Ativação de Conta", mail.subject
-    assert_equal [user.email], mail.to
+    assert_equal "Ativação de Conta",     mail.subject
+    assert_equal [user.email],            mail.to
     assert_equal ["user@realdomain.com"], mail.from
-    assert_match user.name,              mail.body.encoded
-    assert_match user.activation_token,  mail.body.encoded
-    assert_match CGI.escape(user.email), mail.body.encoded
+    assert_match user.name,               mail.body.encoded
+    assert_match user.activation_token,   mail.body.encoded
+    assert_match CGI.escape(user.email),  mail.body.encoded
   end
 
   test "password_reset" do
